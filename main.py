3bc64e2db303b1727a5174c65e290132796c5e5e
wallacce135 <wallacce135@gmail.com>
2023-12-18 14:20:56 +0000

    Link fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from itertools import repeat
 
 def pingDomain(domain, token):
     try:
-        result = urllib.request.urlopen(url="https://" + domain['name'], timeout=35)
+        result = urllib.request.urlopen(url="https://" + domain['name'], timeout=60)
         # print(result)
         # print(domain['name'])
 
@@ -21,13 +21,13 @@ def pingDomain(domain, token):
         print(f"Data was not retrieved {error}\nURL: {domain['name']}")
         if(domain['buyer']['telegramLogin'] is None):
             if(domain['buyer']['name'] is None):
-                text = f"💀{domain['name']}💀 data was not retrieved\nError:TimeoutError\n{domain['owner']['name']}"
+                text = f"💀 {domain['name']} 💀 data was not retrieved\nError:TimeoutError\n{domain['owner']['name']}"
                 requests.get(f"https://api.telegram.org/bot{token}/sendMessage?chat_id=-1001832929433&text={text}")
             else:
-                text = f"💀{domain['name']}💀 data was not retrieved\nError:TimeoutError\n{domain['buyer']['name']}"
+                text = f"💀 {domain['name']} 💀 data was not retrieved\nError:TimeoutError\n{domain['buyer']['name']}"
                 requests.get(f"https://api.telegram.org/bot{token}/sendMessage?chat_id=-1001832929433&text={text}")
         else:
-            text = f"💀{domain['name']}💀 data was not retrieved\nError:TimeoutError\n{domain['buyer']['telegramLogin']}"
+            text = f"💀 {domain['name']} 💀 data was not retrieved\nError:TimeoutError\n{domain['buyer']['telegramLogin']}"
             requests.get(f"https://api.telegram.org/bot{token}/sendMessage?chat_id=-1001832929433&text={text}")
         
 
@@ -36,13 +36,13 @@ def pingDomain(domain, token):
         print(domain['name'] + "-- dead")
         if(domain['buyer']['telegramLogin'] is None):
             if(domain['buyer']['name'] is None):
-                text = f"💀{domain['name']}💀 data was not retrieved\nError:TimeoutError\n{domain['owner']['name']}"
+                text = f" 💀{domain['name']} 💀 data was not retrieved\nError:TimeoutError\n{domain['owner']['name']}"
                 requests.get(f"https://api.telegram.org/bot{token}/sendMessage?chat_id=-1001832929433&text={text}")
             else:
-                text = f"💀{domain['name']}💀 data was not retrieved\nError:TimeoutError\n{domain['buyer']['name']}"
+                text = f"💀 {domain['name']} 💀 data was not retrieved\nError:TimeoutError\n{domain['buyer']['name']}"
                 requests.get(f"https://api.telegram.org/bot{token}/sendMessage?chat_id=-1001832929433&text={text}")
         else:
-            text = f"💀{domain['name']}💀 data was not retrieved\nError:TimeoutError\n{domain['buyer']['telegramLogin']}"
+            text = f"💀 {domain['name']} 💀 data was not retrieved\nError:TimeoutError\n{domain['buyer']['telegramLogin']}"
             requests.get(f"https://api.telegram.org/bot{token}/sendMessage?chat_id=-1001832929433&text={text}")
         requests.get(f"https://api.telegram.org/bot{token}/sendMessage?chat_id=-1001832929433&text={text}")
     return
